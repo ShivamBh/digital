9b58903e82b951ec66452d5b3745d0f8cd9f9d2a
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-04-18 02:43:48 +0530

    model manager products

--- src/products/models.py
+++ src/products/models.py
@@ -1,6 +1,20 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
+
+class ProductQuerySet(models.query.QuerySet):
+	def active(self):
+		return self.filter(active=True)
+
+class ProductManager(models.Manager):
+	def get_queryset(self):
+		return ProductQuerySet(self.model, using=self._db)
+
+	def all(self, *args, **kwargs):
+		return self.get_queryset().active()
+
+
 class Product(models.Model):
 	title = models.CharField(max_length=120)
 	description = models.TextField(blank=True, null=True)
@@ -8,6 +22,13 @@ class Product(models.Model):
 	active = models.BooleanField(default=True)
 	#slug
 	#inventory
+	#image
+	#category
+
+	objects = ProductManager()
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse("product_detail", kwargs={"pk":self.pk})
