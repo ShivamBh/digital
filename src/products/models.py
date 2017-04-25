62470d2f3349d77fb89fd7fd83317930b9106ca3
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-04-26 03:13:35 +0530

    product images added

--- src/products/models.py
+++ src/products/models.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.db.models.signals import post_save
+from django.utils.text import slugify
 from django.db import models
 
 # Create your models here.
@@ -65,7 +66,32 @@ def product_post_saved_receiver(sender, instance, created, *args, **kwargs):
 		new_var.title = "Default"
 		new_var.price = product.price
 		new_var.save()
-	print (variations)
-	print (created)
+	# print (variations)
+	# print (created)
 
 post_save.connect(product_post_saved_receiver, sender=Product)
+
+
+# Product Images
+
+def image_upload_to(instance, filename):
+	title = instance.product.title
+	slug = slugify(title)
+	file_extension = filename.split(".")[1]
+	new_filename = "%s.%s" %(instance.id, file_extension)
+	return "products/%s/%s" %(slug, new_filename)
+
+class ProductImage(models.Model):
+	product = models.ForeignKey(Product)
+	image = models.ImageField(upload_to=image_upload_to)
+
+	def __str__(self):
+		return self.product.title
+
+
+# Product Categories
+
+
+
+
+
