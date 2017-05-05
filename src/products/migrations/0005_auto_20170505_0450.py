4764ffd43f53a648531b85cda0a17db181904c42
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-05-05 10:33:43 +0530

    product category models added

---
+++ src/products/migrations/0005_auto_20170505_0450.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import products.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0004_productimage'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('title', models.CharField(unique=True, max_length=120)),
+                ('slug', models.SlugField(unique=True)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('active', models.BooleanField(default=True)),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+        migrations.AlterField(
+            model_name='productimage',
+            name='image',
+            field=models.ImageField(upload_to=products.models.image_upload_to),
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='categories',
+            field=models.ManyToManyField(to='products.Category', blank=True),
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='default',
+            field=models.ForeignKey(null=True, to='products.Category', blank=True, related_name='default_category'),
+        ),
+    ]
